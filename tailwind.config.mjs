0261a84f7796e29528854c5f479c24ea432b81e3
Anway Kapoor <anwaykapoor2311@gmail.com>
2024-12-14 19:11:36 +0530

    changes in the styles and fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,8 +9,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-        serif: ["Lora", ...defaultTheme.fontFamily.serif],
+        sans: ["Urbanist", ...defaultTheme.fontFamily.sans],
+        serif: ["Urbanist", ...defaultTheme.fontFamily.serif],
       },
     },
   },
